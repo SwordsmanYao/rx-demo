4bbdcf784c769b19e06cf8a7cfa7631f1a061092
姚健 <yaojian@yaojiandeMacBook-Pro.local>
2019-04-16 00:00:21 +0800

    complete

--- breakout/index.js
+++ breakout/index.js
@@ -12,7 +12,6 @@ const BALL_RADIUS = 10;
 // 砖块
 const BRICK_ROWS = 5;
 const BRICK_COLUMNS = 7;
-const BRICK_WIDTH = 80;
 const BRICK_HEIGHT = 20;
 const BRICK_GAP = 3;
 
@@ -41,6 +40,7 @@ function drawScore (score) {
 
 // 球拍
 function drawPaddle (position) {
+  context.clearRect(0, canvas.height - PADDLE_HEIGHT, canvas.width, canvas.height);
   context.beginPath();
   context.rect(
     position - PADDLE_WIDTH / 2,
@@ -64,10 +64,10 @@ function drawBall (ball) {
 function drawBrick(brick) {
   context.beginPath();
   context.rect(
-    brick.x - BRICK_WIDTH / 2,
-    brick.y - BRICK_HEIGHT/ 2,
-    BRICK_WIDTH,
-    BRICK_HEIGHT
+    brick.x - brick.width / 2,
+    brick.y - brick.height/ 2,
+    brick.width,
+    brick.height
   );
   context.fill();
   context.closePath();
@@ -77,19 +77,179 @@ function drawBricks(bricks) {
   bricks.forEach(brick => drawBrick(brick));
 }
 
+const TICKER_INTERVAL = Math.ceil(1000 / 60);
 
+// 时钟输入
+const ticker$ = Rx.Observable.interval(TICKER_INTERVAL, Rx.Scheduler.requestAnimationFrame)
+  .map(() => ({
+    time: Date.now(),
+    deltaTime: null
+  }))
+  .scan((previous, current) => ({
+    time: current.time,
+    deltaTime: (current.time - previous.time) / 1000  // 时间差值，用于计算小球和木板的具体位置（js中的定时器并不能保证时间间隔准确）
+  }));
+
+const PADDLE_CONTROLS = {
+  ArrowLeft: -1,
+  ArrowRight: 1
+};
+
+// 键盘输入
+const key$ = Rx.Observable.merge(
+  Rx.Observable.fromEvent(document, 'keydown').map(event => PADDLE_CONTROLS[event.key] || 0),
+  Rx.Observable.fromEvent(document, 'keyup').map(event => 0)
+)
+  .distinctUntilChanged(); // 保险起见，做一下去重
+
+const PADDLE_SPEED = 240;
+
+// 产生球拍位置的流
+const createPaddle$ = ticker$ => ticker$
+  .withLatestFrom(key$)
+  .scan((position, [ticker, direction]) => {
+    const nextPosition = position + direction * ticker.deltaTime * PADDLE_SPEED;
+    return Math.max(
+      Math.min(nextPosition, canvas.width - PADDLE_WIDTH / 2),
+      PADDLE_WIDTH / 2
+    );
+  }, canvas.width / 2)
+  .distinctUntilChanged();
+
+// 判断球和木板是否碰撞
+function isHit(paddle, ball) {
+  return ball.position.x > paddle - PADDLE_WIDTH / 2
+    && ball.position.x < paddle + PADDLE_WIDTH / 2
+    && ball.position.y > canvas.height - PADDLE_HEIGHT - BALL_RADIUS / 2;
+}
+
+// 判断球和砖块是否碰撞
+function isCollision(brick, ball) {
+  return ball.position.x + ball.direction.x > brick.x - brick.width / 2
+    && ball.position.x + ball.direction.x < brick.x  + brick.width / 2
+    && ball.position.y + ball.direction.y > brick.y - brick.height / 2
+    && ball.position.y + ball.direction.y < brick.y + brick.height / 2;
+}
+
+// 初始化状态
+const initState = () => ({
+  ball: {
+    position: {
+      x: canvas.width / 2,
+      y: canvas.height / 2
+    },
+    direction: {
+      x: 2,
+      y: 2
+    }
+  },
+  bricks: createBricks(),
+  score: 0
+});
+
+// 初始化砖块
+function createBricks() {
+  let width = (canvas.width - BRICK_GAP - BRICK_GAP * BRICK_COLUMNS) / BRICK_COLUMNS;
+  let bricks = [];
+  for (let i = 0; i < BRICK_ROWS; i++) {
+    for(let j = 0; j < BRICK_COLUMNS; j++) {
+      bricks.push({
+        x: j * (width + BRICK_GAP) + width / 2 + BRICK_GAP,
+        y: i * (BRICK_HEIGHT + BRICK_GAP) + BRICK_HEIGHT / 2 + BRICK_GAP + 20,
+        width: width,
+        height: BRICK_HEIGHT
+      });
+    }
+  }
+  return bricks;
+}
+
+const BALL_SPEED = 60;
+
+const createState$ = (ticker$, paddle$) =>
+  ticker$
+  .withLatestFrom(paddle$)
+  .scan(({ ball, bricks, score}, [ticker, paddle]) => {
+    let remainingBricks = [];
+    const collisions = {
+      paddle: false,
+      floor: false,
+      wall: false,
+      ceiling: false,
+      brick: false
+    };
+
+    ball.position.x = ball.position.x + ball.direction.x * ticker.deltaTime * BALL_SPEED;
+    ball.position.y = ball.position.y + ball.direction.y * ticker.deltaTime * BALL_SPEED;
+
+    bricks.forEach(brick => {
+      if (!isCollision(brick, ball)) {
+        remainingBricks.push(brick);
+      } else {
+        collisions.brick = true;
+        score = score + 10;
+      }
+    });
+
+    collisions.paddle = isHit(paddle, ball);
+
+    if (ball.position.x < BALL_RADIUS || ball.position.x > canvas.width - BALL_RADIUS) {
+      ball.direction.x = - ball.direction.x;
+      collisions.wall = true;
+    }
+
+    collisions.ceiling = ball.position.y < BALL_RADIUS;
+
+    if (collisions.brick || collisions.paddle || collisions.ceiling) {
+      ball.direction.y = - ball.direction.y;
+    }
+
+    return {
+      ball: ball,
+      bricks: remainingBricks,
+      collisions: collisions,
+      score: score
+    };
+  }, initState());
+
+function updateView([ticker, paddle, state]) {
+  context.clearRect(0, 0, canvas.width, canvas.height);
+
+  drawPaddle(paddle);
+  drawBall(state.ball);
+  drawBricks(state.bricks);
+  drawScore(state.score);
+
+  if (state.ball.position.y > canvas.height - BALL_RADIUS) {
+    drawGameOver('GAME OVER');
+    restart.error('game over');
+  }
+
+  if (state.bricks.length === 0) {
+    drawGameOver('Congradulations');
+    restart.error('cong');
+  }
+}
+
+let restart;
 
 (function (){
-  drawIntro();
-  drawGameOver('Game Over');
-  drawScore(16);
-  drawPaddle(100);
-  drawBall({position: { x: 100, y: 100 }});
+  const game$ = Rx.Observable.create(observer => {
+    drawIntro();
 
-  drawBricks([{
-    x: 50,
-    y: 50
-  }])
+    restart = new Rx.Subject();
+
+    const paddle$ = createPaddle$(ticker$);
+    const state$ = createState$(ticker$,  paddle$);
+  
+    ticker$.withLatestFrom(paddle$, state$)
+      .merge(restart)
+      .subscribe(observer);
+  });
+
+  game$.retryWhen(err$ => {
+    return err$.delay(1000);
+  }).subscribe(updateView);
 })();
 
 
